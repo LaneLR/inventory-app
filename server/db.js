a5762a63bc698f19d8f8e66c5d1027b2e2f8573a
LaneLR <laneleerichardson@gmail.com>
2026-03-14 19:54:16 -0500

    upgrade to Postgres from SQLite3

--- server/db.js
+++ server/db.js
@@ -1,10 +1,16 @@
-const path = require("path");
-const { Sequelize } = require("sequelize");
+const { Sequelize } = require('sequelize');
+const pg = require('pg');
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectModule: pg,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
   logging: false,
-  storage: path.join(__dirname, "db.sqlite"),
 });
 
 module.exports = sequelize;
