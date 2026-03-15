083cdba725c7b1dee869cf9fa532d0b0897ce9e4
LaneLR <laneleerichardson@gmail.com>
2026-03-14 20:50:50 -0500

    update payload

--- public/react/components/AddForm.jsx
+++ public/react/components/AddForm.jsx
@@ -1,10 +1,10 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState } from "react";
 import styled from "styled-components";
 import apiURL from "../api";
-import Card from './Card';
+import Card from "./Card";
 import Button from "./Button";
-import { useNavigate } from 'react-router-dom';
-import { AllStatesContext } from './App';
+import { useNavigate } from "react-router-dom";
+import { AllStatesContext } from "./App";
 
 const Wrapper = styled.div`
   display: flex;
@@ -14,7 +14,6 @@ const Wrapper = styled.div`
   width: 70%;
 `;
 
-
 const FormWrapper = styled.div`
   display: flex;
   justify-content: left;
@@ -22,7 +21,7 @@ const FormWrapper = styled.div`
   flex-flow: column nowrap;
   margin-top: 20px;
   gap: 5px;
-  width: 300px; 
+  width: 300px;
 `;
 
 const Title = styled.h3`
@@ -40,7 +39,7 @@ const Preview = styled.div`
 `;
 
 const StyledInput = styled.input`
-  width: 100%; 
+  width: 100%;
   padding: 10px;
   margin-bottom: 8px;
   border: 1px solid #ccc;
@@ -55,7 +54,7 @@ const StyledInput = styled.input`
 `;
 
 const StyledTextarea = styled.textarea`
-  width: 100%; 
+  width: 100%;
   padding: 10px;
   margin-bottom: 8px;
   border: 1px solid #ccc;
@@ -71,40 +70,53 @@ const StyledTextarea = styled.textarea`
   }
 `;
 
-
 function AddForm() {
-  const { handleItemAdded } = useContext(AllStatesContext)
-  const navigate = useNavigate()
+  const { handleItemAdded } = useContext(AllStatesContext);
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
-    name: '',
+    name: "",
     price: 0,
     quantity: 0,
-    description: '',
-    category: '',
-    image: ''
+    description: "",
+    category: "",
+    image: "",
   });
 
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const payload = {
+      ...formData,
+      price: parseFloat(formData.price),
+      quantity: parseInt(formData.quantity),
+    };
+
     try {
       const res = await fetch(`${apiURL}/items`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(payload), //
       });
       const data = await res.json();
       if (res.ok) {
-        handleItemAdded(data); 
+        handleItemAdded(data);
         alert(`Your item "${formData.name}" added successfully!`);
-        setFormData({ name: '', price: 0, quantity: 0, description: '', category: '', image: '' });
-        navigate('/')
+        setFormData({
+          name: "",
+          price: 0,
+          quantity: 0,
+          description: "",
+          category: "",
+          image: "",
+        });
+        navigate("/");
       } else {
         alert("Error: " + data.error);
       }
@@ -113,35 +125,77 @@ function AddForm() {
     }
   };
 
-  return (<>
-    <Wrapper>
-      <form onSubmit={handleSubmit}>  
-        <FormWrapper>
-          <Title>Add New Item</Title>
-          <StyledInput name="name" placeholder="Name" value={formData.name} onChange={handleChange} required />
-          <StyledInput step={0.01} name="price" min={0} type="number" placeholder="Price" value={formData.price} onChange={handleChange} required />
-          <StyledInput name="quantity" min={0} type="number" placeholder="Quantity" value={formData.quantity} onChange={handleChange} required />
-          <StyledTextarea name="description" placeholder="Description" value={formData.description} onChange={handleChange} required />
-          <StyledInput name="category" placeholder="Category" value={formData.category} onChange={handleChange} required />
-          <StyledInput name="image" placeholder="Image URL" value={formData.image} onChange={handleChange} required />
-          <Button type="submit">Add Item</Button>
-          
-        </FormWrapper>
-        
-      </form>
-      <Preview>
-        <Title> Preview: </Title>
-        <Card item={{
-          name: formData.name,
-          price: formData.price,
-          quantity: formData.quantity,
-          description: formData.description,
-          category: formData.category,
-          image: formData.image
-        }} />
-      </Preview>
-    </Wrapper>
-      </>
+  return (
+    <>
+      <Wrapper>
+        <form onSubmit={handleSubmit}>
+          <FormWrapper>
+            <Title>Add New Item</Title>
+            <StyledInput
+              name="name"
+              placeholder="Name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+            <StyledInput
+              step={0.01}
+              name="price"
+              min={0}
+              type="number"
+              placeholder="Price"
+              value={formData.price}
+              onChange={handleChange}
+              required
+            />
+            <StyledInput
+              name="quantity"
+              min={0}
+              type="number"
+              placeholder="Quantity"
+              value={formData.quantity}
+              onChange={handleChange}
+              required
+            />
+            <StyledTextarea
+              name="description"
+              placeholder="Description"
+              value={formData.description}
+              onChange={handleChange}
+              required
+            />
+            <StyledInput
+              name="category"
+              placeholder="Category"
+              value={formData.category}
+              onChange={handleChange}
+              required
+            />
+            <StyledInput
+              name="image"
+              placeholder="Image URL"
+              value={formData.image}
+              onChange={handleChange}
+              required
+            />
+            <Button type="submit">Add Item</Button>
+          </FormWrapper>
+        </form>
+        <Preview>
+          <Title> Preview: </Title>
+          <Card
+            item={{
+              name: formData.name,
+              price: formData.price,
+              quantity: formData.quantity,
+              description: formData.description,
+              category: formData.category,
+              image: formData.image,
+            }}
+          />
+        </Preview>
+      </Wrapper>
+    </>
   );
 }
 
